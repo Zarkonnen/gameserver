9d26612d10ced500872e6f92f464e748cdc1ac86
Zarkonnen <david.stark@zarkonnen.com>
2013-06-05 22:44:08 +0200

    Creating games.

--- models.py
+++ models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Game(models.Model):
+    SETUP = 'SE'
+    PLAYING = 'PL'
+    FINISHED = 'FI'
+    STATE_CHOICES = (
+        (SETUP, 'Setup'),
+        (PLAYING, 'Playing'),
+        (FINISHED, 'Finished')
+    )
+    name = models.CharField(max_length=1000)
+    max_players = models.IntField()
+    created = models.DateTimeField(auto_now_add=True)
+    state = models.CharField(max_length=2, choices=STATE_CHOICES, default=SETUP)
+
+class Player(models.Model):
+    name = models.CharField(max_length=1000)
+    game = models.ForeignKey("Game")
+    ready = models.BooleanField(default=False)
+
+class Message(models.Model):
+    data = models.TextField()
+    game = models.ForeignKey("Game")
+    sender = models.ForeignKey("Player")
--- views.py
+++ views.py
@@ -1,1 +1,26 @@
-# Create your views here.
+from models import Game, Player, Message
+from django.http import HttpResponse
+import json
+
+def api(request):
+    q = json.loads(request.POST['json'])
+    action = q['action']
+    response = {'success': False}
+    if action == 'create':
+        g = Game(name=q['name'], max_players=q['max_players'])
+        g.save()
+        response['id'] = g.pk
+        response['success'] = True
+    if action == 'list':
+        pass
+    if action == 'join':
+        pass
+    if action == 'ready':
+        pass
+    if action == 'leave':
+        pass
+    if action == 'send':
+        pass
+    if action == 'poll':
+        pass
+    return HttpResponse(json.dumps(response))
